f67052c189b768267f984f4b9457c0a4c8743d76
hyungkyu <hyungkyu@deepeyes.co.uk>
2021-04-01 11:05:05 +0100

    interface and sorting excercises

---
+++ Day19.Interfaces/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Day19.Interfaces
+{
+    public interface AdvancedArithmetic{
+        int divisorSum(int n);
+    }
+    
+    public class Calculator : AdvancedArithmetic
+    {
+        public int divisorSum(int n)
+        {
+            var dividor = 0;
+            var share = 0;
+            var totalSum = 0;
+            while (dividor + 1 <= n / (dividor + 1))
+            {
+                dividor += 1;
+                if (n % dividor != 0) continue;
+
+                share = n / dividor;
+                totalSum += (dividor == share) ? dividor : dividor + share;
+            }
+            return totalSum;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            AdvancedArithmetic myCalculator = new Calculator();
+            int sum = myCalculator.divisorSum(n);
+            Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum); 
+        }
+    }
+}
---
+++ Day20.Sorting/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Day20.Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp,Int32.Parse);
+
+            var totalSwaps = 0;
+            while (true)
+            {
+                var numSwaps = 0;
+                for (var i = 0; i < n - 1; i++)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        // Swap(ref a[i], ref a[i + 1]);
+                        (a[i], a[i + 1]) = (a[i + 1], a[i]);
+                        numSwaps++;
+                        totalSwaps++;
+                    }
+                }
+
+                if (numSwaps == 0)
+                {
+                    break;
+                }
+            }
+            
+            Console.WriteLine($"Array is sorted in {totalSwaps} swaps.");
+            Console.WriteLine($"First Element: {a[0]}");
+            Console.WriteLine($"Last Element: {a[n-1]}");
+        }
+
+        private static void Swap<T>(ref T x, ref T y)
+        {
+            T temp = y;
+            y = x;
+            x = temp;
+        }
+    }
+}
